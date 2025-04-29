9c5ddebe1042b98acfb37d0d718787912bcd6783
Nat <nanat2727@hotmail.com>
2025-04-29 19:03:09 +0700

    Fix: add jsconfig for path alias and missing files

---
+++ components/users/page.jsx
@@ -0,0 +1,16 @@
+// components/UsersPage.jsx
+import React from 'react';
+
+const UsersPage = () => {
+  return (
+    <div>
+      <h1>รายชื่อผู้ใช้</h1>
+      <ul>
+        <li>ผู้ใช้ 1</li>
+        <li>ผู้ใช้ 2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default UsersPage;
---
+++ server.ts
@@ -0,0 +1,21 @@
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+ 
+const port = parseInt(process.env.PORT || '2222', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+ 
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+ 
+  console.log(
+    `> Server listening at https://watkhoathong.com:2222/ as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
